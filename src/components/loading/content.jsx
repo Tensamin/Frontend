a3f8b055bb60a2ccd14e17de82be3bf18a547ad0
Alois <moaugustin@proton.me>
2025-07-30 16:00:55 +0200

    Added Cool Animation

--- src/components/loading/content.jsx
+++ src/components/loading/content.jsx
@@ -1,8 +1,16 @@
 // Package Imports
 import Image from "next/image";
+import { useEffect } from "react";
+import { useDencrypt } from "use-dencrypt-effect";
 
 // Main
 export function Loading({ message }) {
+    let [coolMessage, setCoolMessage] = useDencrypt({ initialValue: btoa("Tensamin"), interval: 15})
+
+    useEffect(() => {
+        setCoolMessage(message || "Loading...")
+    }, [])
+
     return (
             <div className="w-screen h-screen bg-[#11111b] flex justify-center items-center flex-col gap-20">
                 <Image
@@ -14,7 +22,7 @@ export function Loading({ message }) {
                     priority
                     unoptimized
                 />
-                <p className="font-bold text-2xl text-white/7">{message || "Authenticating..."}</p>
+                <p className="font-bold font-mono text-2xl text-[#c8ccf4]/30">{coolMessage}</p>
             </div>
     );
 }
