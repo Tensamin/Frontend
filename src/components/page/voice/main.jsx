30973ebcfa9599cb3e625307be6b1d6ac4c4d3ec
Alois <moaugustin@proton.me>
2025-08-06 00:52:40 +0200

    Now adding user widgets in voice call

--- src/components/page/voice/main.jsx
+++ src/components/page/voice/main.jsx
@@ -1,14 +1,13 @@
 // Package Imports
 import React, { useEffect, useState, useMemo, useCallback } from "react";
 import * as Icon from "lucide-react";
+import Image from "next/image"
 
 // Lib Imports
-import { sha256, log } from "@/lib/utils";
+import { convertDisplayNameToInitials } from "@/lib/utils"
 
 // Context Imports
 import { useUsersContext } from "@/components/context/users";
-import { useWebSocketContext } from "@/components/context/websocket";
-import { useEncryptionContext } from "@/components/context/encryption";
 import { useCallContext } from "@/components/context/call";
 
 // Components
@@ -18,21 +17,24 @@ import {
   CommandEmpty,
   CommandGroup,
   CommandInput,
-  CommandItem,
   CommandList,
 } from "@/components/ui/command";
-import { InviteItem, User } from "@/components/page/voice/parts";
+import { Card } from "@/components/ui/card"
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import { Skeleton } from "@/components/ui/skeleton";
+import { InviteItem, VideoStream } from "@/components/page/voice/parts";
 
 // Main
 export function Main() {
   let { ownUuid, chatsArray } = useUsersContext();
-  let { connected } = useCallContext();
+  let { connected, connectedUsers, streamingUsers } = useCallContext();
   let [inviteOpen, setInviteOpen] = useState(false);
   let [focused, setFocused] = useState("");
 
   return (
     <div className="flex flex-col gap-1 h-full w-full">
       <div className="flex gap-1 w-full justify-start">
+        {/* Connection Status Button */}
         <Button
           className={`gap-2 ${connected ? "" : "bg-destructive hover:bg-destructive/90"}`}
         >
@@ -75,6 +77,84 @@ export function Main() {
           </CommandList>
         </CommandDialog>
       </div>
+      <div className="flex">
+        {connectedUsers.map((user) => (
+          <button
+            key={user}
+            onClick={() => {
+              setFocused(user)
+            }}
+          >
+            <VoiceModal
+              id={user}
+              streams={streamingUsers.includes(user)}
+              focused={focused === user}
+            />
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
+
+function VoiceModal({ id, streams = false, focused }) {
+  let [avatar, setAvatar] = useState("...");
+  let [username, setUsername] = useState("...");
+  let [display, setDisplay] = useState("...");
+  let { get, ownUuid } = useUsersContext();
+  let { getScreenStream } = useCallContext();
+
+  useEffect(() => {
+    if (id !== "") get(id)
+      .then(data => {
+        setAvatar(data.avatar);
+        setUsername(data.username);
+        setDisplay(data.display);
+      });
+  }, [id])
+
+  return (
+    <Card className="p-2 w-auto rounded-3xl">
+      <div className="flex flex-col gap-2">
+        {streams && (
+          <div className="w-[240px] h-[135px]">
+            <VideoStream className="rounded-2xl border-1" peerConnection={id === ownUuid ? getScreenStream() : getScreenStream(id)} local={id === ownUuid} />
+          </div>
+        )}
+        <div className="flex items-center gap-2 p-2 border-1 rounded-2xl bg-input/20">
+          <div>
+            {avatar !== "..." ? (
+              <Avatar className="size-8 bg-accent/50">
+                {avatar !== "" ? (
+                  <Image
+                    className="object-fill"
+                    data-slot="avatar-image"
+                    width={100}
+                    height={100}
+                    src={avatar}
+                    alt=""
+                    onError={() => {
+                      setAvatar("")
+                    }}
+                  />
+                ) : null}
+                <AvatarFallback>
+                  {convertDisplayNameToInitials(username)}
+                </AvatarFallback>
+              </Avatar>
+            ) : (
+              <Skeleton className="rounded-full size-8" />
+            )}
+          </div>
+          <div>
+            {display !== "..." ?
+              <p>{display}</p>
+              :
+              <Skeleton className="mr-20"><p className="invisible">🥴</p></Skeleton>
+            }
+          </div>
+        </div>
+      </div>
+    </Card>
+  )
+}
