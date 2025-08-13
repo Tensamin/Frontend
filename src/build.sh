db8e805188aba35c38734b5a6625637a7b030fea
Aloisianer <moaugustin@proton.me>
2025-08-13 19:20:47 +0200

    Added Electron Building

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,6 @@
+sudo rm -rf node_modules dist package-lock.json
+npm i
+
 sudo docker run --rm -ti \
  --env ELECTRON_CACHE="/root/.cache/electron" \
  --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
@@ -5,6 +8,4 @@ sudo docker run --rm -ti \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
  electronuserland/builder:wine \
- npm run dist
-
-# --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
+ npm run electron-build
---
+++ src/package.sh
@@ -0,0 +1,14 @@
+sudo mkdir ./release -p
+
+# Windows & Debian
+sudo cp "$PWD/dist/tensamin-frontend Setup $npm_package_version.exe" ./release/$npm_package_version-Tensamin.exe -f
+sudo cp $PWD/dist/tensamin-frontend_$npm_package_version'_amd64.deb' ./release/$npm_package_version-Tensamin-amd64.deb -f
+sudo cp $PWD/dist/tensamin-frontend_$npm_package_version'_arm64.deb' ./release/$npm_package_version-Tensamin-arm64.deb -f
+
+sudo cp "$PWD/dist/tensamin-frontend Setup $npm_package_version.exe" ./release/Tensamin.exe -f
+sudo cp $PWD/dist/tensamin-frontend_$npm_package_version'_amd64.deb' ./release/Tensamin-amd64.deb -f
+sudo cp $PWD/dist/tensamin-frontend_$npm_package_version'_arm64.deb' ./release/Tensamin-arm64.deb -f
+
+# Arch
+#sudo cp "$PWD/dist/tensamin-frontend-$npm_package_version.tar.xz" ./release/aur/amd64.tar.xz
+#sudo cp "$PWD/dist/tensamin-frontend-$npm_package_version-arm64.tar.xz" ./release/aur/arm64.tar.xz
