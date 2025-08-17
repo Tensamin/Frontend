835082b4312b38cd7a825f3e111189dd8fb96e9f
Aloisianer <moaugustin@proton.me>
2025-08-17 12:54:54 +0200

    Added icons

--- forge.config.cjs
+++ forge.config.cjs
@@ -10,14 +10,19 @@ module.exports = {
         options: {
           maintainer: 'Methanium',
           homepage: 'https://methanium.net'
-        }
+        },
+        icon: './public/web-app-manifest-512x512.png'
       }
     },
     {
       name: '@electron-forge/maker-zip'
     },
     {
-      name: '@electron-forge/maker-squirrel'
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        iconUrl: 'https://tensamin.methanium.net/windows-256x256.ico',
+        setupIcon: './src/app/favicon.ico'
+      }
     }
   ]
 };
--- src/app/manifest.js
+++ src/app/manifest.js
@@ -4,20 +4,22 @@ export default function manifest() {
     short_name: 'Tensamin',
     description: 'Super secure messaging app',
     start_url: '/',
-    display: 'fullscreen',
-    background_color: '#101013',
-    theme_color: '#101013',
+    display: 'standalone',
+    background_color: '#1e1e2e',
+    theme_color: '#1e1e2e',
     icons: [
       {
-        src: '/web-app-manifest-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable"
       },
       {
-        src: '/web-app-manifest-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable"
+      }
+    ]
   }
 }
