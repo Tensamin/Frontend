fd90252c463496654bce5230716c3c6e25139800
Aloisianer <moaugustin@proton.me>
2025-08-17 15:42:31 +0200

    Stuff

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   packagerConfig: {
-    name: "tensamin",
+    name: 'tensamin',
     asar: true,
     ignore: [
       /\.git/,
       /\.next/,
       /out/,
-    ]
+    ],
   },
   makers: [
     {
@@ -14,20 +14,28 @@ module.exports = {
       config: {
         options: {
           maintainer: 'Methanium',
-          homepage: 'https://methanium.net'
+          homepage: 'https://methanium.net',
         },
-        icon: './public/web-app-manifest-512x512.png'
-      }
+        icon: './public/web-app-manifest-512x512.png',
+      },
     },
     {
-      name: '@electron-forge/maker-zip'
+      name: '@electron-forge/maker-zip',
+      // explicitly include win32 so zip is available as a make target on Windows
+      platforms: ['win32', 'linux'],
     },
     {
       name: '@electron-forge/maker-squirrel',
+      platforms: ['win32'],
       config: {
         iconUrl: 'https://tensamin.methanium.net/windows-256x256.ico',
-        setupIcon: './src/app/favicon.ico'
-      }
-    }
-  ]
+        setupIcon: './src/app/favicon.ico',
+      },
+    },
+    {
+      // add the WiX maker as an additional Windows target (MSI)
+      name: '@electron-forge/maker-wix',
+      platforms: ['win32'],
+    },
+  ],
 };
