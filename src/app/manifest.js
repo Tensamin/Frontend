9bfedd1fb893ccdc0bb4d63d31cb29bd283dba28
Aloisianer <moaugustin@proton.me>
2025-08-17 21:07:16 +0200

    Reove thing

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -11,14 +11,12 @@ export default function manifest() {
       {
         src: "/192x192.png",
         sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable"
+        type: "image/png"
       },
       {
         src: "/512x512.png",
         sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable"
+        type: "image/png"
       }
     ]
   }
