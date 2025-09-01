6249d015c451b15c8dbdd5f0eeaf7eaf72a1507f
Alois <moaugustin@proton.me>
2025-09-01 15:24:12 +0200

    Updated install

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export let metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${jetbrainsMono.variable} antialiased`}>
+      <body className={`${jetbrainsMono.variable} antialiased bg-transparent`}>
         {children}
       </body>
     </html>
